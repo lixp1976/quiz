eae65c9a12e9543b6fc9bcde061be74693beec0a
Dmitry Dorofeev <dorofeev@road404.ru>
2014-11-20 00:29:26 +0300

    fixed #1 добавил меню "Настройки", вытащил туда пункт "Аккаунты"

--- sources/accounts/urls.py
+++ sources/accounts/urls.py
@@ -6,4 +6,8 @@ __author__ = 'djud'
 urlpatterns = patterns('accounts.views',
     url('^login/$', 'login_user', name='url_login'),
     url('^logout/$', 'logout_user', name='url_logout'),
+
+    url('^accounts/$', 'list_accounts', name='url_list_accounts'),
+    url('^account/delete/(?P<account_id>\d+)/$', 'delete_account',
+        name='url_delete_account'),
 )
--- sources/accounts/views.py
+++ sources/accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import logout, login
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render_to_response
+from django.template import RequestContext
 
 __author__ = 'djud'
 
@@ -19,3 +21,16 @@ def logout_user(request):
     logger.info('[<- LOGOUT] %s' % request.user.username)
     logout(request)
     return redirect("/")
+
+
+def list_accounts(request):
+    context = {
+        'accounts': User.objects.all()
+    }
+    return render_to_response('accounts/list.html', context,
+                              context_instance=RequestContext(request))
+
+
+def delete_account(request, account_id):
+    User.objects.get(id=account_id).delete()
+    return redirect(list_accounts)
